e41d65f41fa1fe5a979c001f62c0ff96de55a0ef
sufianWG <webgurumrmbd@gmail.com>
2026-09-02 20:47:58 +0600

    added proxy to protect specific routes

---
+++ src/proxy.js
@@ -0,0 +1,19 @@
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+
+    const session = await auth.api.getSession({
+        headers: await headers() 
+    })
+
+    if(!session){
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+}
+
+export const config = {
+    matcher: ['/tutors/:path*','/profile','/my-tutors','/my-booked-sessions','/add-tutor']
+}
