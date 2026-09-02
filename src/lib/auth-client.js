03a025b7fa2e2439bc616e11a50f469cd565b16c
sufianWG <webgurumrmbd@gmail.com>
2026-09-02 10:17:15 +0600

    resolved the 'jwt is not defined' issue

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,3 +1,4 @@
+import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(`${process.env.MONGODB_URL}`);
 const db = client.db("tutorcueusers");
